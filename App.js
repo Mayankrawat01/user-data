07db64217a94296efa48d4187e9d3c8a60456e43
Mayankrawat01 <108858368+Mayankrawat01@users.noreply.github.com>
2023-08-29 19:22:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import UserTable from './component/UserTable';
+import './App.css';
+import LoginPage from './component/LoginPage';
+
+function App() {
+  return (
+    <div>
+       
+      {/* <UserTable /> */}
+   <LoginPage />
+
+    </div>
+  )
+}
+export default App;
